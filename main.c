f5c0ae23f3ba6a91edf0104942c9a7d6f5d467e0
gameblabla <gameblabla@protonmail.com>
2021-03-25 09:07:51 +0100

    Add Windowed mode that works.

--- main.c
+++ main.c
@@ -13,10 +13,21 @@ A minimal starting point for 4k GDI demo/game programming.
 #include <mmsystem.h>
 #include <stdint.h>
 #include <stdbool.h>
-#include "main.h"
 
 #include "s.h"
 #include "pal.h"
+
+HINSTANCE g_hInstance;
+HWND g_hWnd;
+HDC g_hDC;
+
+#define DEFAULT_WINDOW_WIDTH 640
+#define DEFAULT_WINDOW_HEIGHT 480
+
+#define INTERNAL_WINDOW_WIDTH 120
+#define INTERNAL_WINDOW_HEIGHT 90
+/* Minimum amount of milliseconds for each update cycle:*/
+#define STEP_RATE 13
 
 #ifdef SIXTEEN_BPP
 BITMAPINFO bi;
@@ -25,12 +36,20 @@ BITMAPINFO* bi;
 #endif
 uint8_t var1;
 
-// Initialization and shutdown:
+// Entry point:
 
-static inline void Initialize()
+void WinMainCRTStartup(VOID)
 {
-	/* It's set to 640x480 but actually, we will detect the screen resolution later */
-    g_hDC = GetDC(CreateWindow("STATIC", 0, WS_POPUP | WS_VISIBLE | WS_MAXIMIZE, 0, 0, 640, 480, 0, 0, 0, 0));
+	MSG msg;
+	
+	/* It's set to 640x480 but actually, we will detect the screen resolution later (unless WINDOWED is set) */
+    g_hDC = GetDC(CreateWindow("STATIC", 0, 
+    #ifdef WINDOWED
+    WS_OVERLAPPED | WS_VISIBLE
+    #else
+    WS_POPUP | WS_VISIBLE | WS_MAXIMIZE
+    #endif
+    , 0, 0, DEFAULT_WINDOW_WIDTH, DEFAULT_WINDOW_HEIGHT + 25, 0, 0, 0, 0));
 
 	#ifdef SIXTEEN_BPP
 	bi.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
@@ -53,30 +72,7 @@ static inline void Initialize()
 		bi->bmiColors[var1].rgbBlue  = pal[(var1*3)+2];
 	}
 	#endif
-}
-
-
- // Update step and main loop:
-
-static inline void Update ()
-{
-    // just paint the whole back buffer in blue:
-    //RECT rcWindow = { 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT };
-    //FillRect(g_hMemDC, &rcWindow, g_hBlue);
 	
-	StretchDIBits(g_hDC,0,0,GetDeviceCaps(g_hDC,HORZRES),GetDeviceCaps(g_hDC,VERTRES),0,0,INTERNAL_WINDOW_WIDTH,INTERNAL_WINDOW_HEIGHT, pict,
-	#ifdef SIXTEEN_BPP
-	&bi,
-	#else
-	bi,
-	#endif
-	DIB_RGB_COLORS, SRCCOPY);
-}
-
-
-static inline void Loop ()
-{
-	MSG msg;
     do
     {
        // DWORD dwStart = timeGetTime();
@@ -85,7 +81,19 @@ static inline void Loop ()
         while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE) != 0) DispatchMessage(&msg);
 		
         // update and copy the back buffer:
-        Update();
+		StretchDIBits(g_hDC,0,0,
+		#ifdef WINDOWED
+		DEFAULT_WINDOW_WIDTH, DEFAULT_WINDOW_HEIGHT,
+		#else
+		GetDeviceCaps(g_hDC,HORZRES),GetDeviceCaps(g_hDC,VERTRES),
+		#endif
+		0,0,INTERNAL_WINDOW_WIDTH,INTERNAL_WINDOW_HEIGHT, pict,
+		#ifdef SIXTEEN_BPP
+		&bi,
+		#else
+		bi,
+		#endif
+		DIB_RGB_COLORS, SRCCOPY);
 
         // sleep until next step:
         /*DWORD dwDelta = timeGetTime() - dwStart;
@@ -93,14 +101,5 @@ static inline void Loop ()
             Sleep(STEP_RATE - dwDelta);
         }*/
     } while(!GetAsyncKeyState(VK_ESCAPE));
-}
-
-
-// Entry point:
-
-void WinMainCRTStartup(VOID)
-{
-	Initialize();
-    Loop();
 }
 
